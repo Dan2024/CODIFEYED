3a0ee27970d2d12d52a2f86f1ab59a7c68d6e016
Dan <dannorthcott8@gmail.com>
2022-09-02 16:06:01 +0100

    another potential fix

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ export default function App() {
   return (
     <>
       <Header />
-      <Routes basename='CODIFEYED'>
-        <Route path='/' element={<HomePage />} />
-        <Route path='algorithm-visualizer' element={<AlgorithmVisualizer />} />
-        <Route path='fractal-forest' element={<FractalForest />} />
-        <Route path='pendulum' element={<PendulumPage />} />
+      <Routes>
+        <Route path='/CODIFEYED' element={<HomePage />} />
+        <Route
+          path='/CODIFEYED/algorithm-visualizer'
+          element={<AlgorithmVisualizer />}
+        />
+        <Route path='/CODIFEYED/fractal-forest' element={<FractalForest />} />
+        <Route path='/CODIFEYED/pendulum' element={<PendulumPage />} />
       </Routes>
       <Background />
     </>
